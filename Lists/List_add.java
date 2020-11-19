0d6cf6937b02c32003827f2a0030edfb9e711647
lokeshyadavcs <thatslokesh1@gmail.com>
2020-11-19 20:48:46 +0530

    add fun commit

---
+++ Lists/List_add.java
@@ -0,0 +1,28 @@
+package Lists;
+
+import java.util.*;
+
+
+/**
+ * List_add
+ */
+public class List_add {
+
+      public static void main(String[] args) {
+        List<String> list =new ArrayList<String>();  
+        list.add("Mango");
+        list.add("papaya");
+        list.add("papaya");
+        list.add("apple");
+        list.add("pineapple");
+        list.add("beans");
+        list.add("chocolate");
+        list.add("candy");
+        list.add("lolipop");  
+        for(String i : list){
+            System.out.println(i);
+            
+      }  
+    
+}
+}
